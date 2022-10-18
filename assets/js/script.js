1f853d47f1b3c2b677721944fd8901b4f11e99c0
Rama Alfin Baehaqi <ramaalfin7@gmail.com>
2022-10-18 20:43:42 +0700

    push

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,52 @@ navLinks.forEach((l) => {
     bsCollapse.toggle();
   });
 });
+
+const modelViewerSofa = document.querySelector("#model-viewer-sofa");
+const modelViewerMeja = document.querySelector("#model-viewer-meja");
+const modelViewerKursi = document.querySelector("#model-viewer-kursi");
+
+window.switchSrcSofa = (element, name) => {
+  const base_glb = "assets/img/Asset_AR/Glb/" + name;
+  const base_png = "assets/img/Asset_AR/Png/" + name;
+  modelViewerSofa.src = base_glb + ".glb";
+  modelViewerSofa.poster = base_png + ".png";
+  const slides = document.querySelectorAll(".slide1");
+  const color = document.querySelectorAll(".color");
+  slides.forEach((element) => {
+    element.classList.remove("selected");
+  });
+  element.classList.add("selected"); 
+};
+
+
+window.switchSrcMeja = (element, name) => {
+  const base_glb = "assets/img/Asset_AR/Glb/" + name;
+  const base_png = "assets/img/Asset_AR/Png/" + name;
+  modelViewerMeja.src = base_glb + ".glb";
+  modelViewerMeja.poster = base_png + ".png";
+  const slides = document.querySelectorAll(".slide1");
+  slides.forEach((element) => {
+    element.classList.remove("selected");
+  });
+  element.classList.add("selected");
+};
+
+window.switchSrcKursi = (element, name) => {
+  const base_glb = "assets/img/Asset_AR/Glb/" + name;
+  const base_png = "assets/img/Asset_AR/Png/" + name;
+  modelViewerKursi.src = base_glb + ".glb";
+  modelViewerKursi.poster = base_png + ".png";
+  const slides = document.querySelectorAll(".slide1");
+  slides.forEach((element) => {
+    element.classList.remove("selected");
+  });
+  element.classList.add("selected");
+};
+
+document.querySelector(".slider").addEventListener("beforexrselect", (ev) => {
+  // Keep slider interactions from affecting the XR scene.
+  ev.preventDefault();
+});
+
+// change color model
